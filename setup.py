9d5bf2ee2e14e5948468c5da4add37c96a1603f7
Florian <1technophile@users.noreply.github.com>
2022-04-03 10:15:10 -0500

    Update version
    and doc

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ except SKBuildError:
 
 setup(
     name="TheengsGateway",
-    version="0.1.5dev",
+    version="0.1.5",
     long_description=long_description,
     long_description_content_type='text/markdown',
     author="Theengs",
