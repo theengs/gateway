586c9f12b96ceae4ab9cf97e9d5d2b15814a6469
Koen Vervloesem <koen@vervloesem.eu>
2023-07-13 19:19:24 +0200

    Add support for encrypted PVVX advertisements (#146)

---
+++ TheengsGateway/decryption.py
@@ -0,0 +1,142 @@
+"""Decryption module for Theengs Gateway.
+
+This module adds decryptors for encrypted advertisements of a selection of
+devices.
+"""
+from abc import ABC, abstractmethod
+import binascii
+from typing import Dict
+
+from Cryptodome.Cipher import AES
+
+
+class AdvertisementDecryptor(ABC):
+    """Abstract class that represents a decryptor for advertisements."""
+
+    @abstractmethod
+    def compute_nonce(self, address: str, decoded_json: Dict) -> bytes:
+        """Compute the nonce for a specific address and JSON input."""
+        pass
+
+    @abstractmethod
+    def decrypt(
+        self, bindkey: bytes, address: str, decoded_json: Dict
+    ) -> bytes:
+        """Decrypt ciphertext from JSON input."""
+        pass
+
+    @abstractmethod
+    def replace_encrypted_data(
+        self, decrypted_data: bytes, data_json: Dict, decoded_json: Dict
+    ) -> None:
+        """Replace the encrypted data by decrypted payload."""
+        pass
+
+
+class LYWSD03MMC_PVVXDecryptor(AdvertisementDecryptor):
+    """Class for decryption of LYWSD03MMX PVVX encrypted advertisements."""
+
+    def compute_nonce(self, address: str, decoded_json: Dict) -> bytes:
+        """Compute the nonce for a specific address and JSON input."""
+        # The nonce consists of:
+        # 6 bytes: device address in reverse
+        # 1 byte : length of (service data + type and UUID)
+        # 1 byte : "16" -> AD type for "Service Data - 16-bit UUID"
+        # 2 bytes: "1a18" -> UUID 181a in little-endian
+        # 1 byte : counter
+        return binascii.unhexlify(
+            "".join(
+                [
+                    reverse_address(address),
+                    bytes([len(decoded_json["servicedata"]) // 2 + 3]).hex(),
+                    "161a18",
+                    decoded_json["ctr"],
+                ]
+            )
+        )
+
+    def decrypt(
+        self, bindkey: bytes, address: str, decoded_json: Dict
+    ) -> bytes:
+        """Decrypt ciphertext from JSON input with AES CCM."""
+        nonce = self.compute_nonce(address, decoded_json)
+        cipher = AES.new(bindkey, AES.MODE_CCM, nonce=nonce, mac_len=4)
+        cipher.update(b"\x11")
+        payload = bytes.fromhex(decoded_json["cipher"])
+        mic = bytes.fromhex(decoded_json["mic"])
+        return cipher.decrypt_and_verify(payload, mic)
+
+    def replace_encrypted_data(
+        self, decrypted_data: bytes, data_json: Dict, decoded_json: Dict
+    ) -> None:
+        """Replace the encrypted data by decrypted payload."""
+        data_json["servicedata"] = decrypted_data.hex()
+
+
+class ShellyBLUButton1Decryptor(AdvertisementDecryptor):
+    """Class for decryption of ShellyBLU Button1 encrypted advertisements."""
+
+    def compute_nonce(self, address: str, decoded_json: Dict) -> bytes:
+        """Compute the nonce for a specific address and JSON input."""
+        # The nonce consists of:
+        # 6 bytes: device address
+        # 2 bytes: "d2fc" -> UUID fcd2 in little-endian
+        # 1 byte : BTHome device info
+        # 1 byte : counter
+        return binascii.unhexlify(
+            "".join(
+                [
+                    address.replace(":", ""),
+                    "d2fc",  # UUID fcd2
+                    decoded_json["servicedata"][:2],
+                    decoded_json["ctr"],
+                ]
+            )
+        )
+
+    def decrypt(
+        self, bindkey: bytes, address: str, decoded_json: Dict
+    ) -> bytes:
+        """Decrypt ciphertext from JSON input with AES CCM."""
+        nonce = self.compute_nonce(address, decoded_json)
+        cipher = AES.new(bindkey, AES.MODE_CCM, nonce=nonce, mac_len=4)
+        payload = bytes.fromhex(decoded_json["cipher"])
+        mic = bytes.fromhex(decoded_json["mic"])
+        return cipher.decrypt_and_verify(payload, mic)
+
+    def replace_encrypted_data(
+        self, decrypted_data: bytes, data_json: Dict, decoded_json: Dict
+    ) -> None:
+        """Replace the encrypted data by decrypted payload."""
+        # Clear encryption and MAC included bits in device info
+        # See https://bthome.io/format/
+        device_info = bytes.fromhex(decoded_json["servicedata"][:2])
+        mask = 0b11111100
+        masked_device_info = int.from_bytes(device_info, "big") & mask
+        bthome_service_data = bytearray(masked_device_info.to_bytes(1, "big"))
+
+        # Replace encrypted data by decrypted payload
+        bthome_service_data.extend(decrypted_data)
+        data_json["servicedata"] = bthome_service_data.hex()
+
+
+_DECRYPTOR_MODELS = {
+    "LYWSD03MMC/MJWSD05MMC_PVVX_ENCR": LYWSD03MMC_PVVXDecryptor,
+    "SBBT_002C_ENCR": ShellyBLUButton1Decryptor,
+}
+
+
+class UnsupportedEncryptionError(Exception):
+    """Exception raised when trying to decrypt an unsupported device."""
+
+
+def create_decryptor(model_id: str) -> AdvertisementDecryptor:
+    """Return the decryptor class for the given model ID."""
+    if model_id not in _DECRYPTOR_MODELS:
+        raise UnsupportedEncryptionError()
+    return _DECRYPTOR_MODELS[model_id]()
+
+
+def reverse_address(address: str) -> str:
+    """Reverse device address bytes and remove colons."""
+    return "".join(address.split(":")[::-1])
